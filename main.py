2a693ed907aa75f41f487a57a9d344ec950bea69
Vadim <va25dim@gmail.com>
2023-11-01 16:37:43 +0300

    My first commit

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-
+def main ():
+    import os
+    return os.getcwd()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print("adsfdsafsadf-", main())
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
